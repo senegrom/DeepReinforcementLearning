b609bcf24b5ec4fa506cc582a17fb1212a234cee
= <6349874+senegrom@users.noreply.github.com>
2021-12-22 18:17:36 +0000

    update

--- settings.py
+++ settings.py
@@ -1,2 +1,5 @@
-run_folder = 'E:/C4/run/'
-run_archive_folder = 'E:/C4/run_archive/'
+# run_folder = 'E:/C4/run/'
+# run_archive_folder = 'E:/C4/run_archive/'
+
+run_folder = './run/'
+run_archive_folder = './run_archive/'
