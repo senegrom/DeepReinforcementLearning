70074e4a0f5b69a4465707e2367b17a820838122
= <6349874+senegrom@users.noreply.github.com>
2022-01-22 13:41:25 +0000

    update

--- initialise.py
+++ initialise.py
@@ -6,8 +6,8 @@ parser = argparse.ArgumentParser(allow_abbrev=False)
 parser.add_argument("-nn", "--run-number", required=False, default=None, type=int)
 parser.add_argument("-mm", "--model-version", required=False, default=None, type=int)
 parser.add_argument("-yy", "--memory-version", required=False, default=None, type=int)
-parser.add_argument("--run-folder", required=False, default=Path('./run/'), type=Path)
-parser.add_argument("--run-archive-folder", required=False, default=Path('./run_archive/'), type=Path)
+parser.add_argument("--run-folder", required=False, default=Path('D:/Connect4/run/'), type=Path)
+parser.add_argument("--run-archive-folder", required=False, default=Path('D:/Connect4/run_archive/'), type=Path)
 
 args, _ = parser.parse_known_args()
 
@@ -17,6 +17,3 @@ INITIAL_MEMORY_VERSION = args.memory_version
 
 run_folder = args.run_folder
 run_archive_folder = args.run_archive_folder
-
-run_folder = Path('E:/C4/run/')
-run_archive_folder = Path('E:/C4/run_archive/')
